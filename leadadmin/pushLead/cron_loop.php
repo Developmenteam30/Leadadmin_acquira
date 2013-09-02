99d14231e66693921a142f9b32f2aa4dbc7837c2
Ryan Smith <ryan@playnicetogether.com>
2013-09-02 20:31:07 +0000

    Setup looping cron

---
+++ leadadmin/pushLead/cron_loop.php
@@ -0,0 +1,17 @@
+<?php
+$cron_version = '9.6';
+include("_f_onlms_v".$cron_version.".php");//all the functions necessary to run the script are included first.
+
+// Loop through the list of active feeds
+if ( ( $result = getActiveFeeds() ) ) {
+	while ($obj = $result->fetch_object()) {
+
+		print "* Processing feed: {$obj->idFeedOut}\n";
+
+		system( sprintf( 'php -f /var/www/html/qmleads.com/leadadmin/pushLead/onlms_process.php -- --v=%s --idFeedOut=%s --cron=1 >/dev/null 2>&1 &',
+							escapeshellarg( $cron_version ),
+							escapeshellarg( $obj->idFeedOut ) 
+						)
+				);
+	}
+}
