97fc9081cd65a50e6910fdbebd538dc3e02769fb
Ryan Smith <ryan@playnicetogether.com>
2014-04-16 00:11:39 +0000

    Fix permissions script

--- fixperms.sh
+++ fixperms.sh
@@ -2,4 +2,6 @@
 chown -R rscs:webdev * .svn
 chmod -R g+rw * .svn
 chown -R apache:webdev error public_html/leadadmin/exports/
+chown -R rscs:webdev public_html/leadadmin/exports/.svn
+chown -R rscs:webdev error/.svn
 chmod -R o+rwx pushLead/lockfiles/
