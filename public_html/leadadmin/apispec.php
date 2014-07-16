ed6e685da8f9ab2a2e17ca6fc52997b40f75fe0e
Ryan Smith <ryan@playnicetogether.com>
2014-07-16 02:17:35 +0000

    Updated API specs page

--- public_html/leadadmin/apispec.php
+++ public_html/leadadmin/apispec.php
@@ -1,458 +1,141 @@
 <?php
-//ADMINROOT/apispec.php
-//Version 1.0
-//ES20130808 Version 1.0: API Spec automatic generation.
-session_start();
-$mysqlErrorSource = 'API Spec Page';
+
 include("../../includes/c_config.php");
-$forceMysqlLogFile = SITE_ROOT."error".FD."log_apispec"; 
-include(INCLUDES."_connx.php");
-include(INCLUDES."loginCheck.php");
-include(INCLUDES."f_site.php");
-include(INCLUDES."c_loginRequired.php"); //Login is required for this page.
 
-if(!$adminLoggedIn){ 
-	if(isset($_REQUEST['a'])){
-		$result = array('status' => 0, 'error'=> 'You are no longer logged in. Log back in and try again.');
-		echo json_encode($result); exit;
-	}
-	elseif(isset($_REQUEST['d'])){
-		echo "You are no longer logged in. Log back in and try again.";	exit;
-	} else { 
-		header("Location: index.php"); exit;
-	}
+require_once( INCLUDES . 'session.php' );
+LeadsSession::requireAccess( LEADS_SESSION_LEVEL_STAFF );
+
+require_once( INCLUDES . 'leads.php' );
+
+if( empty( $_REQUEST['idFeedIn'] ) ) {
+	die('ERROR: Please specify a feed id.');
 }
 
-if(isset($_REQUEST['idFeedIn'])){ 
-	$feed = getIncomingFeed($_REQUEST['idFeedIn']);
-	$company = getCompany($feed->idCompany);
-?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<HTML>
-<HEAD>
-	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=utf-8">
-	<TITLE></TITLE>
-	<META NAME="GENERATOR" CONTENT="OpenOffice.org 3.1  (Win32)">
-	<STYLE TYPE="text/css">
-	<!--
-		@page { margin: 0.79in }
-		TD P { margin-bottom: 0in }
-		P { margin-bottom: 0.08in }
-	-->
-	</STYLE>
-</HEAD>
-<BODY DIR="LTR">
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif"><B><?php echo CONFIG_COMPANY_NAME; ?></B></FONT></P>
-<P STYLE="margin-bottom: 0in"><BR>
-</P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif"><B>Lead
-Submission API Specifications</B></FONT></P>
-<P STYLE="margin-bottom: 0in"><BR>
-</P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif"><B>For:
-</B></FONT><FONT FACE="Verdana, sans-serif"><SPAN STYLE="font-weight: normal"><?php echo $company->name; ?></SPAN>
-</FONT></P>
-<P STYLE="margin-bottom: 0in"><BR>
-</P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">The
-lead submission system performs on a key-value pair submission via
-HTTP POST or HTTP GET. An XML response is produced after an attempt
-to post a lead to the system.</FONT></P>
-<P STYLE="margin-bottom: 0in"><BR>
-</P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif"><B>Posting
-URL</B></FONT><FONT FACE="Verdana, sans-serif">: 
-https://<?php echo SITE_URL; ?>/<?php echo LIVE_FOLDER; ?>/<?php echo $feed->label; ?>/livefeed.php</FONT></P>
-<P STYLE="margin-bottom: 0in"><BR>
-</P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">Use
-the following listcodes for these URLs: </FONT>
-</P>
-</P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">Allowed
-Fields</FONT></P>
-<TABLE WIDTH=666 BORDER=1 BORDERCOLOR="#000000" CELLPADDING=4 CELLSPACING=0>
-	<COL WIDTH=66>
-	<COL WIDTH=100>
-	<COL WIDTH=62>
-	<COL WIDTH=165>
-	<COL WIDTH=231>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Field</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Type</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Required</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Format
-			Requirements</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Description</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>pswd</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(16)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Yes</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><BR>
-			</P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2><?php echo $feed->password; ?></FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>listcode</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(20)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><BR>
-			</P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Campaign ID or
-			List Descriptor</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>url</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(500)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Yes</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><BR>
-			</P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Source of the
-			Lead</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>ip</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(16)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Yes</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><BR>
-			</P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>IP Address</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>stamp</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>datetime</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Yes</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>YYYY-mm-dd
-			HH:ii:ss</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Lead Action Date</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>email</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(150)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Yes</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><BR>
-			</P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Email Address</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>fname</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(50)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><BR>
-			</P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>First Name</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>lname</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(50)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><BR>
-			</P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Last Name</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>addr</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(150)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><BR>
-			</P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Address Line 1</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>addr2</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(150)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><BR>
-			</P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Address Line 2</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>city</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(75)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><BR>
-			</P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>City</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>state</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(25)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>XX</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>State</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>zip</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(20)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>#####</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Zip</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>country</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>char(2)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>XX</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>2-letter ISO-3166 country code</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>dob</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>date</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>YYYY-mm-dd</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Date of Birth</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>gender</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(10)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>M, F</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Gender</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>landline</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(20)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>##########</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Default Phone</FONT></FONT></P>
-		</TD>
-	</TR>
-	<TR VALIGN=TOP>
-		<TD WIDTH=66>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>cellphone</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=100>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>varchar(20)</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=62>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>No</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=165>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>##########</FONT></FONT></P>
-		</TD>
-		<TD WIDTH=231>
-			<P><FONT FACE="Verdana, sans-serif"><FONT SIZE=2>Alternate Phone</FONT></FONT></P>
-		</TD>
-	</TR>
-</TABLE>
-<P STYLE="margin-bottom: 0in"><BR>
-</P>
-<P STYLE="margin-bottom: 0in; page-break-before: always"><BR>
-</P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif"><B>Valid
-Response Example</B></FONT></P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">&lt;?xml
-version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">&lt;response&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in">  <FONT FACE="Verdana, sans-serif">&lt;success&gt;true&lt;/success&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in">  <FONT FACE="Verdana, sans-serif">&lt;reason&gt;Successfully
-inserted new record.&lt;/reason&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">&lt;/response&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in"><BR>
-</P>
-<P STYLE="margin-bottom: 0in"><BR>
-</P>
-<P STYLE="margin-bottom: 0in"><BR>
-</P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif"><B>Invalid
-Response Examples</B></FONT></P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">&lt;?xml
-version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">&lt;response&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in">  <FONT FACE="Verdana, sans-serif">&lt;success&gt;false&lt;/success&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in">  <FONT FACE="Verdana, sans-serif">&lt;reason&gt;Unauthorized
-access.&lt;/reason&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">&lt;/response&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in"><BR>
-</P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">&lt;?xml
-version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">&lt;response&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in">  <FONT FACE="Verdana, sans-serif">&lt;success&gt;false&lt;/success&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in">  <FONT FACE="Verdana, sans-serif">&lt;reason&gt;Email
-is a required field, and may not be empty.&lt;/reason&gt;</FONT></P>
-<P STYLE="margin-bottom: 0in"><FONT FACE="Verdana, sans-serif">&lt;/response&gt;</FONT></P>
-</BODY>
-</HTML>
-<?php
+$leads = Leads::getInstance();
+$feed = $leads->getInboundFeed( $_REQUEST['idFeedIn'] );
+if( empty( $feed ) ) {
+	die('ERROR: Feed not found.');
 }
+
+$company = $leads->getCompany( $feed['idCompany'] );
+
+$fields = array(
+	'pswd' => array( 'type' => 'varchar(16)', 'format' => '', 'notes' => $feed['password'] ),
+	'listcode' => array( 'type' => 'varchar(20)', 'format' => '', 'notes' => 'Campaign ID or List Descriptor' ),
+	'url' => array( 'type' => 'varchar(255)', 'format' => '', 'notes' => 'Source of the lead' ),
+	'ip' => array( 'type' => 'varchar(16)', 'format' => '', 'notes' => 'IP Address' ),
+	'stamp' => array( 'type' => 'datetime', 'format' => '', 'notes' => 'Lead action date' ),
+	'email' => array( 'type' => 'varchar(255)', 'format' => '', 'notes' => 'Email address' ),
+	'fname' => array( 'type' => 'varchar(50)', 'format' => '', 'notes' => 'First name' ),
+	'lname' => array( 'type' => 'varchar(50)', 'format' => '', 'notes' => 'Last name' ),
+	'addr' => array( 'type' => 'varchar(150)', 'format' => '', 'notes' => 'Address line 1' ),
+	'addr2' => array( 'type' => 'varchar(150)', 'format' => '', 'notes' => 'Address line 2' ),
+	'city' => array( 'type' => 'varchar(75)', 'format' => '', 'notes' => 'City' ),
+	'state' => array( 'type' => 'varchar(25)', 'format' => 'XX', 'notes' => 'State/Province' ),
+	'zip' => array( 'type' => 'varchar(120)', 'format' => '#####', 'notes' => 'Zip or Postal code' ),
+	'country' => array( 'type' => 'char(2)', 'format' => 'XX', 'notes' => '2-letter ISO-3166 country code' ),
+	'dob' => array( 'type' => 'date', 'format' => 'YYYY-mm-dd', 'notes' => 'Date of birth' ),
+	'gender' => array( 'type' => 'char(1)', 'format' => 'M, F', 'notes' => 'Gender' ),
+	'landline' => array( 'type' => 'varchar(20)', 'format' => '##########', 'notes' => 'Default phone' ),
+	'cellphone' => array( 'type' => 'varchar(20)', 'format' => '##########', 'notes' => 'Alternate phone' ),
+);
+
+$requiredArray = explode( ';', 'pswd;' . $feed['required'] );
+$allowedArray = explode( ';', 'pswd;' . $feed['allowedFields'] );
+
 ?>
+<!DOCTYPE html>
+<html lang="en-US" prefix="og: http://ogp.me/ns#">
+
+<head>
+<meta charset="UTF-8" />
+<title>API Specifications - <?php echo $company['name']; ?></title>
+<style type="text/css">
+body {
+	font-family: Verdana, sans-serif;
+}
+
+table {
+	border-collapse:collapse;
+	page-break-after: always;
+}
+
+table td {
+	border: 1px solid #000;
+	padding: 5px;
+}
+
+thead td {
+	font-weight: bold;
+	text-align: center;
+}
+</style>
+</head>
+
+<body>
+
+<h1><?php echo CONFIG_COMPANY_NAME; ?></h1>
+
+<h2>Lead Submission API Specifications</h2>
+
+<h3>Company: <?php echo $company['name']; ?> (Feed: <?php echo $feed['idFeedIn'] ?>)</h3>
+
+<p>The lead submission system works on a key-value pair submission via HTTP POST or HTTP GET. An XML response is produced after an attempt to post a lead to the system.  All submissions must use SSL over HTTPS.</p>
+
+<h4>API Field Definitions</h4>
+
+<p><strong>API URL:</strong> https://www.<?php echo SITE_URL; ?>/<?php echo LIVE_FOLDER; ?>/<?php echo $feed['label']; ?>/livefeed.php</p>
+
+<table>
+	<thead>
+	<tr>
+		<td>Field</td>
+		<td>Type</td>
+		<td>Required</td>
+		<td>Format</td>
+		<td>Notes</td>
+	</tr>
+	</thead>
+	<tbody>
+<?php
+	foreach( $allowedArray as $allowed ) {
+?>
+	<tr>
+		<td><?php echo $allowed; ?></td>
+		<td><?php echo $fields[$allowed]['type']; ?></td>
+		<td><?php echo in_array( $allowed, $requiredArray ) ? 'Yes' : 'No'; ?></td>
+		<td><?php echo $fields[$allowed]['format']; ?></td>
+		<td><?php echo $fields[$allowed]['notes']; ?></td>
+	</tr>
+<?php
+	}
+?>
+	</tbody>
+</table>
+
+<h4>API Responses</h4>
+
+<h5>Valid Response Examples</h5>
+
+<p>&lt;?xml version="1.0" encoding="UTF-8"?&gt;<br/>
+&lt;response&gt;<br/>
+&lt;success&gt;true&lt;/success&gt;<br/>
+&lt;reason&gt;Successfully inserted new record.&lt;/reason&gt;<br/>
+&lt;/response&gt;</p>
+
+<h5>Invalid Response Examples</h5>
+
+<p>&lt;?xml version="1.0" encoding="UTF-8"?&gt;<br/>
+&lt;response&gt;<br/>
+&lt;success&gt;false&lt;/success&gt;<br/>
+&lt;reason&gt;Unauthorized access.&lt;/reason&gt;<br/>
+&lt;/response&gt;</p>
+
+<p>&lt;?xml version="1.0" encoding="UTF-8"?&gt;<br/>
+&lt;response&gt;<br/>
+&lt;success&gt;false&lt;/success&gt;<br/>
+&lt;reason&gt;Email is a required field, and may not be empty.&lt;/reason&gt;<br/>
+&lt;/response&gt;</p>
+
+</body>
+</html>
