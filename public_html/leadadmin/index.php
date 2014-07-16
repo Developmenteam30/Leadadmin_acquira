5d254b20654951b733e5dec621ae7f35ed87e6ce
Ryan Smith <ryan@playnicetogether.com>
2014-07-16 03:27:53 +0000

    New login page

--- public_html/leadadmin/index.php
+++ public_html/leadadmin/index.php
@@ -1,11 +1,14 @@
 <?php 
-//ADMIN_ROOT/index.php
-session_start();
-$mysqlErrorSource = 'Index/Login';
-include("../../includes/c_config.php");
-include(INCLUDES."_connx.php");
-include(INCLUDES."f_site.php");
-include( INCLUDES . 'session.php' );
+
+require_once( '../../includes/c_config.php' );
+require_once( INCLUDES . 'session.php' );
+
+if( LeadsSession::isValid( LEADS_SESSION_LEVEL_STAFF ) ) {
+	header("location: dashboard.php"); exit;
+}
+
+require_once( INCLUDES . 'leads.php' );
+$leads = Leads::getInstance();
 
 if(isset($_REQUEST['a'])){ 
 	$result = array(
@@ -23,60 +26,22 @@ if(isset($_REQUEST['a'])){
 				if(!$c){ break; }
 			}
 			if($c){
-				$username 		= $_REQUEST['username'];
-				$password 		= $_REQUEST['password'];
-				
-				$username = stripslashes($username);
-				$password = stripslashes($password);
-				dbCon();
-				$username = $GLOBALS['dbconnx']->escape_string($username);
-				$password = $GLOBALS['dbconnx']->escape_string($password);
-				
-				include(INCLUDES."PasswordHash.php");
-				$pHasher = new PasswordHash(12, false);
-				$getUser = "SELECT * FROM `".DATABASE_NAME."`.`users` "
-					."WHERE `username` = '". $username ."';";
-				$dogetUser = dbQry($getUser, 'Fetching user for login', true);
-				if($dogetUser === false) { 
-					$c = false; $result['error'] = 'Could not connect to database.';
-				}
-			}
-			if($c){ 
-				if($dogetUser->num_rows == 0) { //Didn't find a matching username, check the password against the 
-					//fake hash so that we match password hashing time for real requests.
-					$fakeHash = "abcdefghijklmnopqrst";
-					$fakeCheck = $pHasher->CheckPassword($password, $fakeHash);
+				if( $leads->verifyUser( $_REQUEST['username'], $_REQUEST['password'] ) ) {
+
+					$result['status'] = 1;
+					$result['error'] = 'Successfully logged in.';
+					LeadsSession::login( $_REQUEST['username'], LEADS_SESSION_LEVEL_ADMIN );
+
+				} else {
+
 					$c = false; $result['error'] = 'Invalid username / password.';
 				}
-			}
-			if($c) { 
-				//Found matching username, check the password against the stored hash
-				$user = $dogetUser->fetch_object();
-				$passwordHash = $user->password;
-				$passwordCheckSuccess = $pHasher->CheckPassword($password, $passwordHash);
-				if(!$passwordCheckSuccess){ 
-					$c = false; $result['error'] = 'Invalid username / password.';
-				}
-			}
-			if($c){ 
-				$result['status'] = 1;
-				$result['error'] = 'Successfully logged in.';
-				LeadsSession::login( $user->idUser, LEADS_SESSION_LEVEL_ADMIN );
+
 			}
 		break;
 	}
 	echo json_encode($result);
 	exit;
-}
-
-if(isset($_REQUEST['d'])){ 
-	switch($_REQUEST['d']){
-	}
-	exit;
-}
-
-if( LeadsSession::isValid( LEADS_SESSION_LEVEL_CLIENT ) ) {
-	header("location: dashboard.php"); exit;
 }
 
 $title = CONFIG_COMPANY_NAME." Admin Panel";
