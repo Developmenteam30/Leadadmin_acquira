ea5b713199c167518f2cc9b4166b5c7f546aa937
Ryan Smith <ryan@playnicetogether.com>
2014-07-16 03:02:01 +0000

    Fix spaces

--- includes/c_header.php
+++ includes/c_header.php
@@ -10,10 +10,11 @@ if(!isset($title)){
 <head>	
 
 <title><?php echo $title; ?></title>
-<link href='style.php' rel='stylesheet' type='text/css' />
+<link href="style.php" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-<script src='//code.jquery.com/jquery-2.0.3.js'></script>
+<script src="//code.jquery.com/jquery-2.0.3.js"></script>
 <script src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-<script src='./js/default.js'></script>
+<script src="./js/default.js"></script>
+
 
 </head>
