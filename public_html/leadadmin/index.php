c1dfe11cba03418b3e30356cea30a4f537d17385
Ryan Smith <ryan@playnicetogether.com>
2014-08-03 02:25:43 +0000

    Fix formatting

--- public_html/leadadmin/index.php
+++ public_html/leadadmin/index.php
@@ -4,26 +4,34 @@ require_once( '../../includes/c_config.php' );
 require_once( INCLUDES . 'session.php' );
 
 if( LeadsSession::isValid( LEADS_SESSION_LEVEL_STAFF ) ) {
-	header("location: dashboard.php"); exit;
+	header("Location: dashboard.php");
+	exit;
 }
 
 require_once( INCLUDES . 'leads.php' );
 $leads = Leads::getInstance();
 
-if(isset($_REQUEST['a'])){ 
+if( isset( $_REQUEST['a'] ) ) { 
 	$result = array(
-		'status' => 0
-		, 'error' => 'Action does not exist.'
+		'status' => 0,
+		'error' => 'Action does not exist.',
 	);
-	switch($_REQUEST['a']){ 		
+
+	switch( $_REQUEST['a'] ) {
+
 		case 'logIn':
-			$c = true; $result['error'] = 'Failed to log in user.';
-			$expectedVars = array('username', 'password');
-			foreach($expectedVars as $expectedVar){ 
-				if(!isset($_REQUEST[$expectedVar]) || empty($_REQUEST[$expectedVar])){ 
-					$c = false; $result['error'] = 'Missing expected value: '.$expectedVar;
+			$c = true;
+			$result['error'] = 'Failed to log in user.';
+			$expectedVars = array( 'username', 'password' );
+
+			foreach( $expectedVars as $expectedVar ) {
+				if( empty( $_REQUEST[$expectedVar] ) ) {
+					$c = false;
+					$result['error'] = 'Missing expected value: '.$expectedVar;
 				}
-				if(!$c){ break; }
+				if(!$c){ 
+					break;
+				}
 			}
 			if($c){
 				if( $leads->verifyUser( $_REQUEST['username'], $_REQUEST['password'] ) ) {
@@ -37,15 +45,18 @@ if(isset($_REQUEST['a'])){
 					$c = false; $result['error'] = 'Invalid username / password.';
 				}
 
+
 			}
+
 		break;
 	}
-	echo json_encode($result);
+
+	echo json_encode( $result );
 	exit;
 }
 
 $title = CONFIG_COMPANY_NAME." Admin Panel";
-include(INCLUDES."c_header.php");
+include( INCLUDES . 'c_header.php' );
 ?>
 <script>
 function logIn(){ 
@@ -76,19 +87,17 @@ function logIn(){
 
 <body>
 
-<div class='mainContainer'>
-	<div class='logoContainer centered loginLogo'><img 
-		src='images/logo.jpg'
-	/></div>
-	<div class='loginContainer'>
-		<h1 class='boxTitle siteHeader'>Log In</h1>
-		<div class='loginBox siteBorder'>
-			<form action='index.php' method='post' onsubmit='logIn(); return false;'>
+<div class="mainContainer">
+	<div class="logoContainer centered loginLogo"><img src="images/logo.jpg"/></div>
+	<div class="loginContainer">
+		<h1 class="boxTitle siteHeader">Log In</h1>
+		<div class="loginBox siteBorder">
+			<form action="index.php" method="post" onsubmit="logIn(); return false;">
 			<p>User:</p>
-			<p class='aCenter'><input id='username' type='text' name='username' class='loginBox' /></p>
+			<p class="aCenter"><input id="username" type="text" name="username" class="loginBox" /></p>
 			<p>Password:</p>
-			<p class='aCenter'><input id='password' type='password' name='password' class='loginBox' /></p>
-			<p class='aRight'><input class='siteButton' type='submit' value='Log In' /></p>
+			<p class="aCenter"><input id="password" type="password" name="password" class="loginBox" /></p>
+			<p class="aRight"><input class="siteButton" type="submit" value="Log In" /></p>
 			</form>
 		</div>
 	</div>
