1b221b7e0e96e8e9e9d9191907b00a245c6f88f6
Ryan Smith <ryan@playnicetogether.com>
2015-09-21 02:35:22 +0000

    Updated archive scripts

--- scripts/archive.php
+++ scripts/archive.php
@@ -10,6 +10,7 @@ $month = new DateTime( '2014-06-01' );
 print "Deleting old errorLog entries: ";
 print $leads->archiveErrors() . PHP_EOL;
 
+/*
 print "Archiving old inbound entries ...\n";
 
 if( ( $feeds = $leads->getInboundFeeds() ) !== null ) {
@@ -18,6 +19,7 @@ if( ( $feeds = $leads->getInboundFeeds() ) !== null ) {
 		print $leads->archiveInbound( $feed->idFeedIn, $month ) . PHP_EOL;
 	}
 }
+*/
 
 /*
 if( ( $tables = $leads->getLegacyInboundTables() ) !== null ) {
@@ -28,15 +30,20 @@ if( ( $tables = $leads->getLegacyInboundTables() ) !== null ) {
 }
 */
 
-print "Deleting old outgoing invalid entries ...\n";
+print "Archiving old outgoing entries ...\n";
 
+if( ( $feeds = $leads->getOutboundFeeds() ) !== null ) {
+	foreach( $feeds as $feed ) {
+		print "\tdata_outbound: " . $feed->idFeedOut . " - ";
+		print $leads->archiveOutbound( $feed->idFeedOut, $month ) . PHP_EOL;
+	}
+}
+
+/*
 if( ( $tables = $leads->getOutboundTables() ) !== null ) {
 	foreach( $tables as $table ) {
-/*
-		print "\tidFeedOut " . $table[2] . ": ";
-		print $leads->archiveOutbound( $table[2], $table[1] ) . PHP_EOL;
-*/
 		print "\t" . $table[0] . ": ";
 		print $leads->archiveLegacyOutbound( $table[0], $table[1] ) . PHP_EOL;
 	}
 }
+*/
