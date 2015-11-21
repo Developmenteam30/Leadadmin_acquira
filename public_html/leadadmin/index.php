26c12a0b64eaf4cd56ab55bd220d41529ae2c125
Ryan Smith <ryan@playnicetogether.com>
2015-11-21 20:22:10 +0000

    Add login debugging

--- public_html/leadadmin/index.php
+++ public_html/leadadmin/index.php
@@ -37,13 +37,18 @@ if( isset( $_REQUEST['a'] ) ) {
 				}
 			}
 			if($c){
+
 				if( ( $user = $leads->verifyUser( $_REQUEST['username'], $_REQUEST['password'] ) ) !== null ) {
+
+$leads->logError( 'SUCCESS ' . print_r($user,true) );
 
 					$result['status'] = 1;
 					$result['error'] = 'Successfully logged in.';
 					LeadsSession::login( $user['idUser'], $user['level'], $user['idCompany'] );
 
 				} else {
+
+$leads->logError( 'FAIL ');
 
 					$c = false; $result['error'] = 'Invalid username / password.';
 				}
