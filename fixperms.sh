22231d4f2275ab0fe1cd353010a3840f02e9c15c
Ryan Smith <ryan@playnicetogether.com>
2016-04-18 01:26:20 +0000

    Remove old uploads and exports after 90 days

--- fixperms.sh
+++ fixperms.sh
@@ -6,3 +6,5 @@ chmod -R g+rw * .svn
 chown -R apache:webdev error public_html/leadadmin/exports/ uploads
 chmod -R g+rw error public_html/leadadmin/exports/
 chmod -R o+rwx pushLead/lockfiles/
+find public_html/leadadmin/exports/* -mtime +90 -exec rm -rf {} \;
+find uploads/* -mtime +90 -exec rm -rf {} \;
