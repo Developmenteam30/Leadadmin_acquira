77815f751c1c7c5d723a66f4ccfa9c963f687dbf
Ryan Smith <ryan@playnicetogether.com>
2016-05-05 19:07:12 +0000

    Fix modal title

--- public_html/leadadmin/ledger.php
+++ public_html/leadadmin/ledger.php
@@ -659,7 +659,7 @@ include(INCLUDES."c_header.php");
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="editcompany_title"><?php echo ( 1 == $type ) ? 'Add a new client invoice' : 'Add a new payment'; ?></h4>
+					<h4 class="modal-title" id="newcompany_title"><?php echo ( 1 == $type ) ? 'Add a new client invoice' : 'Add a new payment'; ?></h4>
 			</div>
 			<div class="modal-body">
 			</div>
