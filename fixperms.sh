47a543a7376d8b42ec94a864b4cfda3bdafbd7ea
Ryan Smith <ryan@playnicetogether.com>
2017-07-17 14:29:56 -0400

    Fix permissions on files

--- fixperms.sh
+++ fixperms.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd $(dirname $0)
 mkdir uploads error public_html/leadadmin/exports pushLead/lockfiles
-chown -R rscs:rscs * .svn
-chmod -R g+rw * .svn
-chown -R apache:webdev error public_html/leadadmin/exports/ uploads
+chown -R rscs:webpriv * .svn
+chmod -R g+rw,o= * .svn
+chown -R nginx:webpriv error public_html/leadadmin/exports/ uploads
 chmod -R g+rw error public_html/leadadmin/exports/
 chmod -R o+rwx pushLead/lockfiles/
 find public_html/leadadmin/exports/* -mtime +90 -exec rm -rf {} \;
