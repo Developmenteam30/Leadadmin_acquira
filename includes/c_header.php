3c6d111d725df93b97805ced72d72a980b80fb8a
Ryan Smith <ryan@playnicetogether.com>
2017-11-06 12:42:55 -0500

    Bump select2 version.

--- includes/c_header.php
+++ includes/c_header.php
@@ -17,13 +17,13 @@ if( !isset( $title ) ) {
 	<link rel="stylesheet" href="/leadadmin/libraries/bootstrap-3.3.6-dist/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="/leadadmin/libraries/bootstrap-3.3.6-dist/css/bootstrap-theme.min.css" />
 	<link rel="stylesheet" href="/leadadmin/libraries/bootstrap-toggle/bootstrap-toggle.min.css" />
-	<link rel="stylesheet" href="/leadadmin/libraries/select2-4.0.3/css/select2.min.css" />
+	<link rel="stylesheet" href="/leadadmin/libraries/select2-4.0.3/dist/css/select2.min.css" />
 	<link href="/v13/leadadmin/style.php" rel="stylesheet" type="text/css" />
 	<script src="/leadadmin/libraries/jquery-2.2.3.min.js"></script>
 	<script src="/leadadmin/libraries/jquery-ui-1.11.4/jquery-ui.min.js"></script>
 	<script src="/leadadmin/libraries/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
 	<script src="/leadadmin/libraries/bootstrap-toggle/bootstrap-toggle.min.js"></script>
-	<script src="/leadadmin/libraries/select2-4.0.3/js/select2.min.js"></script>
+	<script src="/leadadmin/libraries/select2-4.0.3/dist/js/select2.min.js"></script>
 	<script src="/leadadmin/libraries/default.js"></script>
 	<script src="/leadadmin/libraries/calx-1.1.4/jquery-calx-1.1.4.min.js" type="text/javascript"></script>
 	<script src="/v2/leadadmin/libraries/tablefilter/tablefilter.js" type="text/javascript"></script>
