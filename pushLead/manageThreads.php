452935a265925e34668a90df8803fbb10680acb0
Ryan Smith <ryan@playnicetogether.com>
2019-12-03 14:23:20 -0500

    Remove manageThreads.sh script and use pgrep to check for an existing process.

--- pushLead/manageThreads.php
+++ pushLead/manageThreads.php
@@ -1,55 +1,72 @@
 <?php
-require_once( "../includes/c_config.php" );
-require_once( INCLUDES . 'leads.php' );
+
+chdir(dirname(__FILE__));
+
+require_once("../includes/c_config.php");
+require_once(INCLUDES . 'leads.php');
 $leads = Leads::getInstance();
 
 $recordsPerRun = 1000;
 $maxThreads = 20;
 
-function countProcesses( $idFeedOut ) {
+// Check to see if the manageThreads process is already running and it's not this process.
+exec("pgrep -f 'php -f manageThreads.php'", $processes);
+if (!empty($processes) && is_array($processes) && sizeOf($processes)) {
+    foreach ($processes as $process) {
+        if (getmypid() != $process) {
+            die('Already running.');
+        }
+    }
+}
 
-	exec( sprintf( "ps auxwww|grep '[p]ushLeads.php %d$'", intval( $idFeedOut ) ), $output );
-	return !empty( $output ) && is_array( $output ) ? sizeOf( $output ) : 0;
+function countProcesses($idFeedOut)
+{
+
+    exec(sprintf("ps auxwww|grep '[p]ushLeads.php %d$'", intval($idFeedOut)), $output);
+
+    return !empty($output) && is_array($output) ? sizeOf($output) : 0;
 
 }
 
-while( true ) {
+while (true) {
 
-	$feeds = $leads->getOutboundFeedsCron( null );
+    $feeds = $leads->getOutboundFeedsCron(null);
 
-	if( !$feeds || !is_array( $feeds ) ) {
-		print "Unable to get feed list\n";
-		die();
-	}
+    if (!$feeds || !is_array($feeds)) {
+        print "Unable to get feed list\n";
+        die();
+    }
 
-	print "\n====================================\n";
+    print "\n====================================\n";
 
-	foreach( $feeds as $feed ) {
+    foreach ($feeds as $feed) {
 
-		$cnt = countProcesses( $feed->idFeedOut );
-		print "Feed: {$feed->idFeedOut}, Processes: {$cnt}\n";
+        $cnt = countProcesses($feed->idFeedOut);
+        print "Feed: {$feed->idFeedOut}, Processes: {$cnt}\n";
 
-		$threads = round( $feed->queued / $recordsPerRun );
-		if( $threads < 1 ) {
-			$threads = 1;
-		} else if( $threads > $maxThreads ) {
-			$threads = $maxThreads;
-		}
+        $threads = round($feed->queued / $recordsPerRun);
+        if ($threads < 1) {
+            $threads = 1;
+        } else {
+            if ($threads > $maxThreads) {
+                $threads = $maxThreads;
+            }
+        }
 
-		print "\tThreads: {$threads}\n";
+        print "\tThreads: {$threads}\n";
 
-		while( $cnt < $threads ) {
-			print "\tSpawning new\n";
+        while ($cnt < $threads) {
+            print "\tSpawning new\n";
 
-			exec( sprintf( 'php -f pushLeads.php %s>/dev/null 2>&1 &',
-					escapeshellarg( $feed->idFeedOut )
-				)
-			);
-			usleep( 500000 );
+            exec(sprintf('php -f pushLeads.php %s>/dev/null 2>&1 &',
+                    escapeshellarg($feed->idFeedOut)
+                )
+            );
+            usleep(500000);
 
-			$cnt++;
-		}
-	}
+            $cnt++;
+        }
+    }
 
-	sleep( 30 );
+    sleep(30);
 }
