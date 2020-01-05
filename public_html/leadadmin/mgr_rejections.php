640e11d12ed21b36c21446886053b588e804f110
Ryan Smith <ryan@playnicetogether.com>
2020-01-04 20:07:25 -0500

    3111: Add button styling to prev/next links.

--- public_html/leadadmin/mgr_rejections.php
+++ public_html/leadadmin/mgr_rejections.php
@@ -64,78 +64,88 @@ $leads = Leads::getInstance();
         } else {
             ?>
 
-			<h3>Last 100 rejections for <?php echo Display::escHtml($type); ?> feed: <?php echo Display::escHtml($label); ?></h3>
+            <h3>Last 100 rejections for <?php echo Display::escHtml($type); ?>
+                feed: <?php echo Display::escHtml($label); ?></h3>
 
-			<?php if ( $offset > 0 ) { ?>
-				<p style="display: inline-block;margin-right:10px;"><?php printf('<a href="mgr_rejections.php?type=%s&amp;id=%s&amp;label=%s&amp;offset=%d">Previous page</a>', urlencode($type), urlencode($id), urlencode($label), intval($offset - 100)); ?></p>
-			<?php } ?>
-			<p style="display: inline-block;"><?php printf('<a href="mgr_rejections.php?type=%s&amp;id=%s&amp;label=%s&amp;offset=%d">Next page</a>', urlencode($type), urlencode($id), urlencode($label), intval($offset + 100)); ?></p>
-			<br>
+            <?php if ($offset > 0) { ?>
+                <p style="display: inline-block;margin-right:10px;"><?php printf('<a class="btn btn-primary btn-sm"href="mgr_rejections.php?type=%s&amp;id=%s&amp;label=%s&amp;offset=%d">Previous page</a>',
+                        urlencode($type), urlencode($id), urlencode($label), intval($offset - 100)); ?></p>
+            <?php } ?>
+            <p style="display: inline-block;"><?php printf('<a class="btn btn-primary btn-sm" href="mgr_rejections.php?type=%s&amp;id=%s&amp;label=%s&amp;offset=%d">Next page</a>',
+                    urlencode($type), urlencode($id), urlencode($label), intval($offset + 100)); ?></p>
+            <br>
 
-			<table class="rejectionsTable">
-				<thead>
-				<tr>
-					<th>Error Timestamp</th>
-					<th colspan="8">Error Message</th>
-					<th>LeadID</th>
-					<th>URL</th>
-					<th>Email</th>
-					<th>Lead Timestamp</th>
-				</tr>
-				<tr>
-					<th>First Name</th>
-					<th>Last Name</th>
-					<th>Address 1</th>
-					<th>Address 2</th>
-					<th>City</th>
-					<th>State</th>
-					<th>Zipcode</th>
-					<th>Country</th>
-					<th>DOB</th>
-					<th>Gender</th>
-					<th>Landline</th>
-					<th>Cellphone</th>
-					<th>IP Address</th>
-				</tr>
-				</thead>
-				<tbody>
+            <table class="rejectionsTable">
+                <thead>
+                <tr>
+                    <th>Error Timestamp</th>
+                    <th colspan="8">Error Message</th>
+                    <th>LeadID</th>
+                    <th>URL</th>
+                    <th>Email</th>
+                    <th>Lead Timestamp</th>
+                </tr>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Address 1</th>
+                    <th>Address 2</th>
+                    <th>City</th>
+                    <th>State</th>
+                    <th>Zipcode</th>
+                    <th>Country</th>
+                    <th>DOB</th>
+                    <th>Gender</th>
+                    <th>Landline</th>
+                    <th>Cellphone</th>
+                    <th>IP Address</th>
+                </tr>
+                </thead>
+                <tbody>
                 <?php foreach ($records as $record) { ?>
-					<tr>
+                    <tr>
                         <?php if ('outbound' == $type) { ?>
-	                        <td><a href="#" data-toggle="modal" data-backdrop="static" data-target="#modal-postparams" data-feed-id="<?php echo intval($id); ?>" data-record-id="<?php echo intval($record['idRecord']); ?>" data-accepted="<?php echo intval($record['accepted']); ?>" data-result="<?php echo Display::escHtml($record['result']); ?>"><?php echo Display::escHtml($record['timestampConverted']); ?></a></td>
+                            <td><a href="#" data-toggle="modal" data-backdrop="static" data-target="#modal-postparams"
+                                   data-feed-id="<?php echo intval($id); ?>"
+                                   data-record-id="<?php echo intval($record['idRecord']); ?>"
+                                   data-accepted="<?php echo intval($record['accepted']); ?>"
+                                   data-result="<?php echo Display::escHtml($record['result']); ?>"><?php echo Display::escHtml($record['timestampConverted']); ?></a>
+                            </td>
                         <?php } else { ?>
-	                        <td><?php echo Display::escHtml($record['timestampConverted']); ?></td>
+                            <td><?php echo Display::escHtml($record['timestampConverted']); ?></td>
                         <?php } ?>
-						<td class="error" colspan="8"><?php echo Display::escHtml($record['result']); ?></td>
-						<td><?php echo Display::escHtml($record['leadId']); ?></td>
-						<td><?php echo Display::escHtml($record['url']); ?></td>
-						<td><?php echo Display::escHtml($record['email']); ?></td>
-						<td><?php echo Display::escHtml($record['leadstamp']); ?></td>
-					</tr>
-					<tr>
-						<td><?php echo Display::escHtml($record['fname']); ?></td>
-						<td><?php echo Display::escHtml($record['lname']); ?></td>
-						<td><?php echo Display::escHtml($record['addr']); ?></td>
-						<td><?php echo Display::escHtml($record['addr2']); ?></td>
-						<td><?php echo Display::escHtml($record['city']); ?></td>
-						<td><?php echo Display::escHtml($record['state']); ?></td>
-						<td><?php echo Display::escHtml($record['zip']); ?></td>
-						<td><?php echo Display::escHtml($record['country']); ?></td>
-						<td><?php echo Display::escHtml($record['dob']); ?></td>
-						<td><?php echo Display::escHtml($record['gender']); ?></td>
-						<td><?php echo Display::escHtml($record['landline']); ?></td>
-						<td><?php echo Display::escHtml($record['cellphone']); ?></td>
-						<td><?php echo Display::escHtml($record['ip']); ?></td>
-					</tr>
+                        <td class="error" colspan="8"><?php echo Display::escHtml($record['result']); ?></td>
+                        <td><?php echo Display::escHtml($record['leadId']); ?></td>
+                        <td><?php echo Display::escHtml($record['url']); ?></td>
+                        <td><?php echo Display::escHtml($record['email']); ?></td>
+                        <td><?php echo Display::escHtml($record['leadstamp']); ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo Display::escHtml($record['fname']); ?></td>
+                        <td><?php echo Display::escHtml($record['lname']); ?></td>
+                        <td><?php echo Display::escHtml($record['addr']); ?></td>
+                        <td><?php echo Display::escHtml($record['addr2']); ?></td>
+                        <td><?php echo Display::escHtml($record['city']); ?></td>
+                        <td><?php echo Display::escHtml($record['state']); ?></td>
+                        <td><?php echo Display::escHtml($record['zip']); ?></td>
+                        <td><?php echo Display::escHtml($record['country']); ?></td>
+                        <td><?php echo Display::escHtml($record['dob']); ?></td>
+                        <td><?php echo Display::escHtml($record['gender']); ?></td>
+                        <td><?php echo Display::escHtml($record['landline']); ?></td>
+                        <td><?php echo Display::escHtml($record['cellphone']); ?></td>
+                        <td><?php echo Display::escHtml($record['ip']); ?></td>
+                    </tr>
                 <?php } //foreach ?>
-				</tbody>
-			</table>
+                </tbody>
+            </table>
 
-			<?php if ( $offset > 0 ) { ?>
-				<p style="display: inline-block;margin-right:10px;"><?php printf('<a href="mgr_rejections.php?type=%s&amp;id=%s&amp;label=%s&amp;offset=%d">Previous page</a>', urlencode($type), urlencode($id), urlencode($label), intval($offset - 100)); ?></p>
-			<?php } ?>
-			<p style="display: inline-block;"><?php printf('<a href="mgr_rejections.php?type=%s&amp;id=%s&amp;label=%s&amp;offset=%d">Next page</a>', urlencode($type), urlencode($id), urlencode($label), intval($offset + 100)); ?></p>
-			<br/>
+            <?php if ($offset > 0) { ?>
+                <p style="display: inline-block;margin-right:10px;"><?php printf('<a class="btn btn-primary btn-sm" href="mgr_rejections.php?type=%s&amp;id=%s&amp;label=%s&amp;offset=%d">Previous page</a>',
+                        urlencode($type), urlencode($id), urlencode($label), intval($offset - 100)); ?></p>
+            <?php } ?>
+            <p style="display: inline-block;"><?php printf('<a class="btn btn-primary btn-sm" href="mgr_rejections.php?type=%s&amp;id=%s&amp;label=%s&amp;offset=%d">Next page</a>',
+                    urlencode($type), urlencode($id), urlencode($label), intval($offset + 100)); ?></p>
+            <br/>
 
         <?php } ?>
     <?php } ?>
@@ -143,45 +153,46 @@ $leads = Leads::getInstance();
 </div><!-- #.mainContainer -->
 
 <div class="modal fade" id="modal-postparams" tabindex="-1" role="dialog" aria-labelledby="postparams_title">
-	<div class="modal-dialog modal-lg" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title" id="postparams_title">View posting parameters</h4>
-			</div>
-			<div class="modal-body">
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-			</div>
-		</div>
-	</div>
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" id="postparams_title">View posting parameters</h4>
+            </div>
+            <div class="modal-body">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script>
-	$('#modal-postparams').on('show.bs.modal', function (e) {
-		var modal = $(this);
-		var idFeedOut = $(e.relatedTarget).data('feed-id');
-		var idRecord = $(e.relatedTarget).data('record-id');
-		var accepted = $(e.relatedTarget).data('accepted');
-		var result = $(e.relatedTarget).attr('data-result'); // Prevent JSON error responses from being parsed
+    $('#modal-postparams').on('show.bs.modal', function (e) {
+        var modal = $(this);
+        var idFeedOut = $(e.relatedTarget).data('feed-id');
+        var idRecord = $(e.relatedTarget).data('record-id');
+        var accepted = $(e.relatedTarget).data('accepted');
+        var result = $(e.relatedTarget).attr('data-result'); // Prevent JSON error responses from being parsed
 
-		$.ajax({
-			cache: false,
-			type: 'POST',
-			url: 'mgr_feedout.php',
-			data: {
-				'd': 'dialog_postparams',
-				'idFeedOut': idFeedOut,
-				'idRecord': idRecord,
-				'accepted': accepted,
-				'result': result
-			},
-			success: function (data) {
-				modal.find('.modal-body').html(data);
-			}
-		});
-	});
+        $.ajax({
+            cache: false,
+            type: 'POST',
+            url: 'mgr_feedout.php',
+            data: {
+                'd': 'dialog_postparams',
+                'idFeedOut': idFeedOut,
+                'idRecord': idRecord,
+                'accepted': accepted,
+                'result': result
+            },
+            success: function (data) {
+                modal.find('.modal-body').html(data);
+            }
+        });
+    });
 </script>
 
 </body>
