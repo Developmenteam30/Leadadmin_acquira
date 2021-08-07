aad5c904b10183b02badca535687b2d8132d87e9
Ryan Smith <ryan@playnicetogether.com>
2021-08-07 00:26:43 -0400

    Update directory permissions.

--- fixperms.sh
+++ fixperms.sh
@@ -3,8 +3,8 @@ cd $(dirname $0)
 mkdir uploads uploads/chunks error public_html/leadadmin/exports pushLead/lockfiles
 chown -R rscs:webdev * .git .gitignore
 chmod -R g+rw,o= * .git .gitignore
-chown -R nginx:webdev error public_html/leadadmin/exports/ uploads
-chmod -R g+rw error public_html/leadadmin/exports/
+chown -R nginx:webdev error public_html/leadadmin/exports/ uploads files
+chmod -R g+rw error public_html/leadadmin/exports/ uploads files
 chmod -R o+rwx pushLead/lockfiles/
 find public_html/leadadmin/exports/* -mtime +90 -exec rm -rf {} \;
 find uploads/* -mtime +90 -exec rm -rf {} \;
