01bb92f8c9a92decccb84c5a90595e171b704911
Ryan Smith <ryan@playnicetogether.com>
2021-09-08 17:15:36 -0400

    5174: Revert files that don't need to be changed.

--- includes/c_header.php
+++ includes/c_header.php
@@ -25,7 +25,7 @@ if( !isset( $title ) ) {
     <?php if( file_exists( SITE_ROOT . 'public_html/assets/css/branding.css' ) ) { ?>
         <link href="/v<?php echo filemtime( SITE_ROOT . 'public_html/assets/css/branding.css' ); ?>/assets/css/branding.css" rel="stylesheet" type="text/css"/>
     <?php } ?>
-	<script src="/leadadmin/libraries/jquery-2.2.3.min.js"></script> 
+	<script src="/leadadmin/libraries/jquery-2.2.3.min.js"></script>
 	<script src="/leadadmin/libraries/jquery-ui-1.11.4/jquery-ui.min.js"></script>
 	<script src="/leadadmin/libraries/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
 	<script src="/leadadmin/libraries/bootstrap-toggle/bootstrap-toggle.min.js"></script>
@@ -34,28 +34,6 @@ if( !isset( $title ) ) {
 	<script src="/leadadmin/libraries/default.js"></script>
 	<script src="/leadadmin/libraries/calx-1.1.4/jquery-calx-1.1.4.min.js" type="text/javascript"></script>
 	<script src="/v2/leadadmin/libraries/tablefilter/tablefilter.js" type="text/javascript"></script>
-
-	<!--link rel="stylesheet" href="./libraries/jquery-ui-themes-1.11.4/themes/smoothness/jquery-ui.css"/>
-	<link rel="stylesheet" href="./libraries/bootstrap-3.3.6-dist/css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="./libraries/bootstrap-3.3.6-dist/css/bootstrap-theme.min.css"/>
-	<link rel="stylesheet" href="./libraries/bootstrap-toggle/bootstrap-toggle.min.css"/>
-	<link rel="stylesheet" href="./libraries/select2-4.0.3/dist/css/select2.min.css"/>
-	<link rel="stylesheet" href="./libraries/fine-uploader/fine-uploader-new.css"/>
-    <?php if( file_exists( SITE_ROOT . 'public_html/assets/css/admin.css' ) ) { ?>
-        <link href="/v<?php echo filemtime( SITE_ROOT . 'public_html/assets/css/admin.css' ); ?>/assets/css/admin.css" rel="stylesheet" type="text/css"/>
-    <?php } ?>
-    <?php if( file_exists( SITE_ROOT . 'public_html/assets/css/branding.css' ) ) { ?>
-        <link href="/v<?php echo filemtime( SITE_ROOT . 'public_html/assets/css/branding.css' ); ?>/assets/css/branding.css" rel="stylesheet" type="text/css"/>
-    <?php } ?>
-	<script src="./libraries/jquery-2.2.3.min.js"></script>
-	<script src="./libraries/jquery-ui-1.11.4/jquery-ui.min.js"></script>
-	<script src="./libraries/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
-	<script src="./libraries/bootstrap-toggle/bootstrap-toggle.min.js"></script>
-	<script src="./libraries/select2-4.0.3/dist/js/select2.min.js"></script>
-	<script src="./libraries/fine-uploader/fine-uploader.js"></script>
-	<script src="./libraries/default.js"></script>
-	<script src="./libraries/calx-1.1.4/jquery-calx-1.1.4.min.js" type="text/javascript"></script>
-	<script src="../v2/leadadmin/libraries/tablefilter/tablefilter.js" type="text/javascript"></script-->
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
